fe8e29034f5ff963db37ba25ab8477eec648e35b
Matt Brown <mdbrown7079@users.noreply.github.com>
2018-02-26 16:02:21 -0800

    started over

---
+++ sonarterm/src/main/java/mdbrown7079_github/sonarterm/WeightConversion.java
@@ -0,0 +1,15 @@
+package mdbrown7079_github.sonarterm;
+
+
+public class WeightConversion {
+
+	public double KilosToPounds (double kilos){
+		
+		return (kilos / 0.454);
+	}
+	
+	public double PoundsToKilos (double pounds){
+		
+		return (pounds * 0.454);
+	}
+}
